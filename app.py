835517f1e30bcdfa4fd26ccd3ab76355b96c408c
slava <yaroslavborysyuk@gmail.com>
2024-06-21 20:09:17 -0400

    Index.html is now working and rendering. Flask app is running

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 from flask import Flask, render_template, redirect, request, session, flash
+from flask_session import Session
 
+
+# Create the flask app
+app = Flask(__name__)
+
+# Create the seesion
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_FILE_DIR"] = "./flask_session_cache"
+Session(app)
+
+
+@app.route("/")
+def index():
+    
+    return render_template("index.html")
